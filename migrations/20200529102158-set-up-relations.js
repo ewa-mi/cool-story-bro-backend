c5a70456685de6db37cfe751284b988a05d59ecf
ewami <efili1989@gmail.com>
2020-05-29 12:39:21 +0200

    set up relations

---
+++ migrations/20200529102158-set-up-relations.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("homepages", "userId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+    await queryInterface.addColumn("stories", "homepageId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "homepages",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn("homepages", "userId");
+    await queryInterface.removeColumn("stories", "todolistId");
+  },
+};
